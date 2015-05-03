5b5adb06e7bfd38252cd9af05bc8de2f3854fa35
ArGxento <ArGxento@gmail.com>
2015-05-04 00:52:11 +0900

    implement Ray::isHit

--- monochrome/object.hpp
+++ monochrome/object.hpp
@@ -4,6 +4,9 @@
 struct Vector {
 	double x = 0.0;
 	double y = 0.0;
+	static double const cross(Vector const& v1, Vector const& v2) {
+		return v1.x * v2.y - v1.y * v2.x;
+	}
 	Vector(double x_, double y_): x(x_), y(y_) {}
 	Vector() {}
 };
@@ -13,7 +16,7 @@ public:
 	static const int width = 64;
 	Vector location;
 	Vector normal;
-	Mirror(Vector location_, Vector normal_): location(location_), normal(normal_) {}
+	Mirror(Vector const location_, Vector const normal_): location(location_), normal(normal_) {}
 private:
 	Mirror() {}
 };
@@ -24,11 +27,39 @@ public:
 	Vector location;
 	Vector vector;
 	bool isHittable;
-	Ray(Vector location_, Vector vector_): location(location_), vector(vector_) {}
+	Ray(Vector const location_, Vector const vector_): location(location_), vector(vector_) {}
 	bool isHit(Mirror const& m) const {
+		double const eps = 0.0001;
+
 		double const vrad = std::atan2(vector.y, vector.x);
 		Vector const rayDiameter(std::cos(vrad) * 2.0 * radius, std::sin(vrad) * 2.0 * radius);
-		Vector const rayStartPos();
+		Vector const rayStartPos(location.x - std::cos(vrad) * radius, location.y - std::sin(vrad) * radius);
+		
+		double const mrad = std::atan2(m.normal.y, m.normal.y) + 3.1415926 / 4.0;
+		Vector const mirrorSurface(std::cos(mrad) * m.width, std::sin(mrad) * m.width);
+		Vector const mirrorStartPos(m.location.x - std::cos(mrad) * (m.width / 2), m.location.y - std::sin(mrad) * (m.width / 2));
+
+		Vector const dist(mirrorStartPos.x - rayStartPos.x, mirrorStartPos.y - rayStartPos.y);
+		double const mirrorCrsRay = Vector::cross(mirrorSurface, rayDiameter);
+		if(std::abs(mirrorCrsRay) < eps) { return false; }
+		
+		double const distCrsMirror = Vector::cross(dist, mirrorSurface);
+		double const distCrsRay = Vector::cross(dist, rayDiameter);
+
+		double const internalyDivPointMirror = distCrsMirror / mirrorCrsRay;
+		double const internalyDivPointRay = distCrsRay / mirrorCrsRay;
+
+		return(
+			internalyDivPointMirror - eps > 0.0 &&
+			internalyDivPointMirror + eps < 1.0 &&
+			internalyDivPointRay - eps > 0.0 &&
+			internalyDivPointRay + eps < 1.0
+		);
+	}
+
+	Vector getRefrectVec(Mirror const& m) const {
+
+
 	}
 private:
 	Ray() {}
